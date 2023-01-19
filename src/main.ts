19dbd4330850587b37894dd0b93f341cc75be18e
powerinv <cambridge_james@foxmail.com>
2023-01-20 04:58:07 +0800

    安装DevUI

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,14 @@
+import DevUI from "vue-devui";
+import "vue-devui/style.css";
+import "@devui-design/icons/icomoon/devui-icon.css";
+import { ThemeServiceInit, infinityTheme } from "devui-theme";
+
 import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
 
 import "./assets/css/index.css";
 
-createApp(App).use(router).mount("#app");
+ThemeServiceInit({ infinityTheme }, "infinityTheme");
+
+createApp(App).use(DevUI).use(router).mount("#app");
