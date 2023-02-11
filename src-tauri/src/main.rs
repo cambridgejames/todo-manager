62319ea4dd6603851e730cee03050af617063d73
powerinv <cambridge_james@foxmail.com>
2023-02-12 02:28:48 +0800

    去除窗口边框，添加窗口阴影

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,10 +1,19 @@
 #![cfg_attr(
-  all(not(debug_assertions), target_os = "windows"),
-  windows_subsystem = "windows"
+    all(not(debug_assertions), target_os = "windows"),
+    windows_subsystem = "windows"
 )]
 
+mod window;
+
+use crate::window::shadow::set_window_shadow;
+
 fn main() {
-  tauri::Builder::default()
-    .run(tauri::generate_context!())
-    .expect("error while running tauri application");
+    tauri::Builder::default()
+        .setup(|app| {
+            set_window_shadow(app, "mainApplication");
+            Ok(())
+        })
+        .invoke_handler(tauri::generate_handler![])
+        .run(tauri::generate_context!())
+        .expect("error while running tauri application");
 }
