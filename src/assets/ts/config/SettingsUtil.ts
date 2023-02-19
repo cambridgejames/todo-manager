c52acb1ed89ee301afe9f2b3eb451fb615f5a56c
powerinv <cambridge_james@foxmail.com>
2023-02-19 22:52:53 +0800

    移除Tauri组件

--- src/assets/ts/config/SettingsUtil.ts
+++ src/assets/ts/config/SettingsUtil.ts
@@ -1,10 +1,5 @@
 import defaultConfigure from "./settings.default.json";
 import { JSONPath } from "jsonpath-plus";
-import { resolveResource } from "@tauri-apps/api/path";
-import { readTextFile } from "@tauri-apps/api/fs";
-
-const resourcePath = await resolveResource("conf/settings.default.json");
-const userConfigure = JSON.parse(await readTextFile(resourcePath));
 
 /**
  * 根据JSONPath获取配置
@@ -12,14 +7,14 @@ const userConfigure = JSON.parse(await readTextFile(resourcePath));
  * @param jsonPath JSONPath
  */
 export const getConfigure = (jsonPath: string): string | undefined => {
-  const result = JSONPath({ path: jsonPath, json: userConfigure, wrap: false });
+  const result = JSONPath({ path: jsonPath, json: {}, wrap: false });
   return result === undefined ? JSONPath({ path: jsonPath, json: defaultConfigure, wrap: false }) : result;
 };
 
 export const setConfigure = (jsonPath: string, confStr: string): void => {
   JSONPath({
     path: jsonPath,
-    json: userConfigure,
+    json: {},
     wrap: false,
     callback: (a, b, c) => {
       console.log(a, b, c);
