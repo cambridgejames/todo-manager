4ed104a5601226cadfc82fdfe6bf2da21c1c88a7
powerinv <cambridge_james@foxmail.com>
2023-02-26 08:18:33 +0800

    支持修改主题设置时全部窗口同时切换主题

--- src/electron/ipc/IpcHandler.ts
+++ src/electron/ipc/IpcHandler.ts
@@ -11,12 +11,14 @@ const IPC_HANDLER_MAP = new Map<IpcRenderChannel, IpcHandleFunc>();
 
 IPC_HANDLER_MAP.set(IpcRenderChannel.CHANGE_THEME, (event, targetTheme: Theme) => {
   const themeData = targetTheme.data;
-  const mainWindow: BrowserWindow = BrowserWindow.getAllWindows()[0];
-  mainWindow.setBackgroundColor(themeData["devui-global-bg-normal"]);
-  mainWindow.setTitleBarOverlay({
-    color: themeData["devui-global-bg"],
-    symbolColor: themeData["tm-window-btn-color"]
-  });
+  const mainWindows: Array<BrowserWindow> = BrowserWindow.getAllWindows();
+  for (const currentWindow of mainWindows) {
+    currentWindow.setBackgroundColor(themeData["devui-global-bg-normal"]);
+    currentWindow.setTitleBarOverlay({
+      color: themeData["devui-global-bg"],
+      symbolColor: themeData["tm-window-btn-color"]
+    });
+  }
 });
 
 export const handleIpc = () => IPC_HANDLER_MAP.forEach((handler: IpcHandleFunc, channel: string) => {
